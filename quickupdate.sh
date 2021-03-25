8415916a9bbb5ca6f94f1eb3c7d559b3e230fc38
Mikpol21 <mikolajpolinski21>
2021-03-25 01:20:54 +0100

    msg

--- quickupdate.sh
+++ quickupdate.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
 msg="Quick meaningless update"
 if [ -z "$1" ]; then
     msg=$1
 fi
+echo -e "\e[35m\tAdding\e[0m"
 git add .
+echo -e "\e[35m\tStatus:\e[0m"
 git status
-git commit -m "$1"
+echo -e "\e[35m\tCommiting:\e[0m"
+git commit -m "msg"
+echo -e "\e[35m\tPushing:\e[0m"
 git push origin main
+echo -e "\e[34m\tDone\e[0m"
